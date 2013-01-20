db99a70358b58e35cd2ca7605801471d57eacaa6
samno1607 <swig.sg@googlemail.com>
2013-01-20 19:12:55 +0000

    Version 2 Updates

--- init.post_boot.sh
+++ init.post_boot.sh
@@ -1,6 +1,31 @@
 #!/system/bin/sh
-# switch CPUfreq from performance to interactive & tune it
-sleep 60
-echo "interactive" >  /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
-echo 20000 > /sys/devices/system/cpu/cpufreq/interactive/timer_rate
-echo 20000 > /sys/devices/system/cpu/cpufreq/interactive/min_sample_time
+# 
+# Authored By samno For Jeckll & Hyde Kernel
+
+# Remount All Partitions With noatime
+for k in $(mount | grep relatime | cut -d " " -f3);
+do
+mount -o remount,noatime,nodiratime,noauto_da_alloc,barrier=0 $k
+done;
+
+# Set IO Scheduler At Boot
+ echo "sio" > /sys/block/mmcblk0/queue/scheduler
+
+# Switch CPUfreq To Hotplug
+ echo "hotplug" >  /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+
+# Post Boot Init.d Support
+mount -o remount,rw /system
+if [ -d /system/etc/init.d ]
+then
+chmod 755 /system/etc/init.d
+else
+mkdir /system/etc/init.d
+chmod 755 /system/etc/init.d
+fi
+
+if [ -d /system/etc/init.d ]; then
+  /sbin/busybox run-parts /system/etc/init.d
+fi;
+
+## End Of File ###
